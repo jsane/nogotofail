c68009f6e3f17baee17267da63b3078a8c827cd3
Jayant Sane <ghjsane@gmail.com>
2017-11-15 07:45:37 -0800

    (Testing only) Disabling rc4 checking temporarily

--- nogotofail/mitm/connection/handlers/data/ssl.py
+++ nogotofail/mitm/connection/handlers/data/ssl.py
@@ -156,11 +156,12 @@ class InsecureCipherDetectionHandler(DataHandler):
                  (", ".join(des_ciphers)))
 
         # Per rfc7465, RC4 ciphers should never be used in TLS
-        rc4_ciphers = [str(c) for c in client_hello.ciphers if "RC4" in str(c)]
-        if rc4_ciphers:
-             self._handle_bad_ciphers(rc4_ciphers,
-                 "Client enabled RC4 TLS/SSL cipher suites %s" %
-                 (", ".join(rc4_ciphers)))
+        # Disabling rc4 checking temporarily
+        # rc4_ciphers = [str(c) for c in client_hello.ciphers if "RC4" in str(c)]
+        # if rc4_ciphers:
+        #      self._handle_bad_ciphers(rc4_ciphers,
+        #          "Client enabled RC4 TLS/SSL cipher suites %s" %
+        #          (", ".join(rc4_ciphers)))
 
         # Ensure client only supports ciphers from this list
          acceptable_ciphers = set([ "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256", "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256", "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384", "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384", "TLS_DHE_RSA_WITH_AES_128_GCM_SHA256", "TLS_DHE_DSS_WITH_AES_128_GCM_SHA256", "TLS_DHE_RSA_WITH_AES_256_GCM_SHA384", "TLS_DHE_DSS_WITH_AES_256_GCM_SHA384", "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256", "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256", "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA", "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA", "TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384", "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384", "TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA", "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA", "TLS_DHE_RSA_WITH_AES_128_CBC_SHA256", "TLS_DHE_RSA_WITH_AES_256_CBC_SHA256", "TLS_DHE_RSA_WITH_AES_128_CBC_SHA", "TLS_DHE_RSA_WITH_AES_256_CBC_SHA", "TLS_DHE_DSS_WITH_AES_128_CBC_SHA256", "TLS_DHE_DSS_WITH_AES_256_CBC_SHA256", "TLS_DHE_DSS_WITH_AES_128_CBC_SHA", "TLS_DHE_DSS_WITH_AES_256_CBC_SHA", "TLS_RSA_WITH_AES_128_GCM_SHA256", "TLS_RSA_WITH_AES_256_GCM_SHA384", "TLS_RSA_WITH_AES_128_CBC_SHA256", "TLS_RSA_WITH_AES_256_CBC_SHA256", "TLS_RSA_WITH_AES_128_CBC_SHA", "TLS_RSA_WITH_AES_256_CBC_SHA" ])
